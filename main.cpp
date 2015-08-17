391771850da592a26fa6bc6876abbc6c558927a7
unknown <jlee@SMC-NY-PC51.squaremilecapital.com>
2015-08-17 19:03:59 -0400

    Initial commit to local git

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
+
+#git
+
+void hello()
+{
+    cout << "Please enter your name\n";
+    string str;
+    cin >> str;
+    cout << "Hello, " << str << "!\n";
+}
+
+void hello2()
+{
+    cout << "Please enter your name\n";
+    string str;
+    getline(cin,str);
+    cout << "Hello, " << str << "!\n";
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    //hello();
+    hello2();
+
+    return a.exec();
+}
